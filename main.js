a373f7cf39242032df53b4659e2013dca22688b5
Wojtek <wojjas@givingit.se>
2018-05-24 00:34:06 +0200

    Shows repository info and contributors list by rendering the DOM

--- main.js
+++ main.js
@@ -1,50 +1,78 @@
-function main(){
-    const HyfReposHttps = 'https://api.github.com/orgs/HackYourFuture/repos';
+function main() {
+  const HyfReposHttps = "https://api.github.com/orgs/HackYourFuture/repos";
 
-    getRepositories(HyfReposHttps, xhrCallback);
-
-    console.log('main!');
-    
+  getDataFromServer(HyfReposHttps, repositoriesCallback);
 }
 
 var repositories = [];
 
 // Callback that handles response from server
-function xhrCallback(data){
-    repositories = JSON.parse(data);
-    console.log(`Received and parsed ${repositories.length} repositories from server.`);
-    showRepositoriesInSelect(repositories);
+function repositoriesCallback(data) {
+  repositories = JSON.parse(data);
+  console.log(
+    `Received and parsed ${repositories.length} repositories from server.`
+  );
+  showRepositoriesInSelect(repositories);
 }
 
-function showRepositoriesInSelect(repositories){
-    const repositoriesSelectElement = document.querySelector('#repositories');
-    repositoriesSelectElement.setAttribute('onchange', "getSelectedRepository(this)");
+function showRepositoriesInSelect(repositories) {
+  const repositoriesSelectElement = document.querySelector("#repositories");
 
-    repositories.forEach(repository => {
-        const optionElement = document.createElement('option');
-        optionElement.setAttribute('value', repository.id);
-        optionElement.innerText = repository.name;
+  repositoriesSelectElement.setAttribute(
+    "onchange",
+    "getContributors(this.value); showRepository(this.value)"
+  );
 
-        repositoriesSelectElement.appendChild(optionElement);
-    });
+  repositories.forEach(repository => {
+    const optionElement = document.createElement("option");
+    optionElement.setAttribute("value", repository.id);
+    optionElement.innerText = repository.name;
+
+    repositoriesSelectElement.appendChild(optionElement);
+  });
 }
 
-function getSelectedRepository(repositoriesSelectElement){
-    const selectedRepository = repositories.filter(repository => {
-        return repository.id === Number.parseInt(repositoriesSelectElement.value);
-    })[0];
+function showRepository(repositoryId) {
+  const selectedRepository = repositories.filter(repository => {
+    return repository.id === Number.parseInt(repositoryId);
+  })[0];
 
-    console.log('Selected repository', selectedRepository);
+  const repositoryInfo = document.querySelector('.repository-info');
+  repositoryInfo.innerHTML = `<strong>Repository:</strong><span>${selectedRepository.name}</span> <br>
+                                <strong>Description:</strong><span>${selectedRepository.description}</span> <br>
+                                <strong>Forks:</strong><span>${selectedRepository.forks}</span> <br>
+                                <strong>Updated:</strong><span>${selectedRepository.updated_at}</span>`;
+}
+
+function getContributors(repositoryId) {
+  const selectedRepository = repositories.filter(repository => {
+    return repository.id === Number.parseInt(repositoryId);
+  })[0];
+
+  getDataFromServer(selectedRepository.contributors_url, data => {
+    showContributors(data);
+  });
+}
+
+function showContributors(contributorsData) {
+  const contributors = JSON.parse(contributorsData);
+  const contributorsListElement = document.querySelector(".contributors-list");
+
+  contributors.forEach(contributor => {
+    const listItemElement = document.createElement("li");
+    listItemElement.innerHTML = `<img width="100px" src="${contributor.avatar_url}"> <span>${contributor.login}</span> <span>${contributor.contributions}</span>`;
+
+    contributorsListElement.appendChild(listItemElement);
+  });
 }
 
 // Function that makes an server request (API call)
-function getRepositories(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
+function getDataFromServer(theUrl, callback) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  };
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous
+  xmlHttp.send(null);
 }
